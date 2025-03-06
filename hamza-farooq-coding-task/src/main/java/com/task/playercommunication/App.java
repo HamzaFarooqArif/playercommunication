1b4df79c6c487812e68dd82a4d519a2766161094
hamzafarooqarif <m.hamza.arif786@gmail.com>
2025-03-06 10:54:49 +0100

    adjusted app

--- hamza-farooq-coding-task/src/main/java/com/task/playercommunication/App.java
+++ hamza-farooq-coding-task/src/main/java/com/task/playercommunication/App.java
@@ -8,21 +8,15 @@ import com.task.playercommunication.network.Server;
  */
 public class App {
     public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Usage: java com.task.playercommunication.App <server|client>");
-            return;
-        }
-
         try {
-            if (args[0].equalsIgnoreCase("server")) {
-                new Server(); // Start the server
-            } else if (args[0].equalsIgnoreCase("client")) {
-                new Client(); // Start the client
-            } else {
-                System.out.println("Invalid argument! Use 'server' or 'client'.");
-            }
+            new Client();
         } catch (Exception e) {
-            e.printStackTrace();
+            try {
+                new Server();
+            }
+            catch (Exception ex) {
+                ex.printStackTrace();
+            }
         }
     }
 }
